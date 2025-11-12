f7501a0bf1b814a0033213e92152ae19b9c30a69
victorpercio <victorperciodeoliveira@gmail.com>
2025-11-12 19:26:11 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,18 +1,1 @@
-<?php
-    
-    $dbhost = 'localhost';
-    $dbusarname = 'root';
-    $dbpassword = '';
-    $dbname = 'formulario';
-
-   $conexao = new mysql($dbhost,$dbusarname,$dbpassword,$dbname)
-
-   if($conexao->connect_errno)
-   {
-    echo "Erro";
-   }
-   else {
-    echo"conexão efetuada com sucesso"
-   }
-?>
 
